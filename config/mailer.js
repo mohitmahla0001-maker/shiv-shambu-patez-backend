60275a38946de5babd4f7e6807935a3767403c17
mohitmahla <mohitmahla001@.com>
2026-08-03 12:06:15 +0530

    Fixed email IPv6 network issue by forcing IPv4

--- config/mailer.js
+++ config/mailer.js
@@ -1,11 +1,14 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
     },
+    family: 4, // 👈 IPv4 force karta hai, IPv6 ka ENETUNREACH issue fix karta hai
 });
 
 module.exports = transporter;
